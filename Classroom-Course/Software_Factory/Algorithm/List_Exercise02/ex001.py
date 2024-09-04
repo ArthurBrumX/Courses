9f0005b63b8a46fe88ab5e1f29352546d67e20e7
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-09-03 23:25:25 -0400

    atualizacao para criar outras listas

---
+++ Classroom-Course/Software_Factory/Algorithm/List_Exercise02/ex001.py
@@ -0,0 +1,14 @@
+# 1 - Faça um algoritmo que leia o nome, a idade, o
+# sexo, o endereço e o telefone.
+# Posteriormente imprima o resultado de cada
+# variável linha abaixo de linha.
+
+nome = input("Digite o Seu nome: ")
+idade = input("Digite Sua Idade: ")
+endereco = input("Digite Seu Endereço: ")
+telefone = input("Digite Seu Telefone: ")
+
+print("Seu nome é: ",nome)
+print("Sua idade é: ",idade)
+print("Seu endereço é: ",endereco)
+print("Seu telefone é: ",telefone)
