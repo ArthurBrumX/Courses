5022b4e99bab0c40bfc8f16f647fd3d602e4760f
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-09-27 00:34:37 -0400

    UP

---
+++ Classroom-Course/Software_Factory/Algorithm/List_Exercise02/ex004.py
@@ -0,0 +1,46 @@
+# 4 - Faça um algoritmo que leia o nome, a placa do carro, o modelo do carro e a cor do carro. 
+# Posteriormente imprima o resultado de cada variável linha abaixo de linha.
+
+print ("=" * 60)
+
+print ("*** Sitema De Fiscalização ***")
+print ("* Preencha os Dados Do Seu Veiculo!! *")
+    # Apresentação Ao Usuário.
+
+print ("=" * 60)
+
+nomeMotorista = str(input("- Qual Nome Completo Do Proprietário: "))
+    # Tipo Da Variavel = Str(String)
+    # Funcao = Entrada De dados
+
+placaVeiculo = str(input("- Qual é a Placa Do Veiculo: "))
+    # Tipo Da Variavel = Str(String)
+    # Funcao = Entrada De dados
+
+modeloVeiculo = str(input("- Qual é o Modelo/Marca Do Veiculo: "))
+    # Tipo Da Variavel = Str(String)
+    # Funcao = Entrada De dados
+
+corVeiculo = str(input("- Qual é a Cor Do Veiculo: "))
+    # Tipo Da Variavel = Str(String)
+    # Funcao = Entrada De dados
+
+
+print("=" * 60)
+
+print("** Nome Completo Titular: {}**".format(nomeMotorista))
+print("** Placa Do Veiculo: {}**".format(placaVeiculo))
+print("** Marca/Modelo Do Veiculo: {}**".format(modeloVeiculo))
+print("** Cor Do Veiculo: {}**".format(corVeiculo))
+
+print("=" * 60)
+
+print("** Fiscalizacao Concluida Com Sucesso!!! **")
+    # Fução = Saida de dados
+    # Despedida Do usuario
+
+print("=" * 60)
+
+
+
+
